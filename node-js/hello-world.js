4bd8f092a6839d516adda0dad0f9a44e0548ce8c
PAUL SEMPIJJA <s.p.lwevuze@gmail.com>
2022-09-06 08:09:14 -0700

    Upload modifeid more

---
+++ node-js/hello-world.js
@@ -0,0 +1,7 @@
+console.log("hello world paul");
+console.log("programming is fun");
+//declaring in javascript
+let myname = "PAUL_SEMPIJJA";
+console.log(myname)
+let num1 = 10;
+console.log (num1)
