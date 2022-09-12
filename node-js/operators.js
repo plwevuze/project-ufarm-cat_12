166553d6dda0d10ccc322566c8f9ae5ab0440a87
PAUL SEMPIJJA <s.p.lwevuze@gmail.com>
2022-09-12 01:56:22 -0700

    contols

---
+++ node-js/operators.js
@@ -0,0 +1,28 @@
+//operators tells the computer what to do with an operand
+//Arithmetic operators that do arithmetic operations
+var num1 = 25;
+var num2 = 2;
+
+var num3 = 50, num4 = 300;
+//addition operator(+)
+console.log(num1 + num2);
+
+//subtraction operator (-)
+console.log(num1 - num2);
+
+//multiplication operator(*)
+console.log(num1 * num2);
+
+//division operator (/)
+
+console.log(num1 / num2);
+
+//remainder/modulus operator (%)
+console.log(num1 % num2);
+
+//escape operators \n-to the next line, \b-back space, \t-space btn the strings
+let firstName = "Paul\n";
+let lastName = "Sempijja";
+
+//console.log(firstName + lastName); (+) is a concatination operator btn strings
+console.log(firstName + lastName);
---
+++ node-js/variables.js
@@ -0,0 +1,19 @@
+//variable syntax
+//use let,var,const---always in small letters wen u are declearing a variable
+//never declare a variable twice
+//== means an assignment operator
+// wen using 'const' it must never be left empty
+//variable names shouldnt start with a number/figure or special characters ie #$%^&@! etc
+//variable names shudnt have space inbetween then
+//use relatable variable names
+//endevour to terminate with  a semi-colon
+//use an underscore (_) or a carmel notation (ie. uppercase) in case a variable name has more than two words
+//not advisable to start/end the name of the variable with an upper case but can be used within for compound name
+//white spaces are allowed in  programming
+//endevour to write comments in ur code
+
+let first_name;
+first_name = "paul"
+const num1 = 10;
+var lastName = "lwevuze";
+console.log(first_name);
